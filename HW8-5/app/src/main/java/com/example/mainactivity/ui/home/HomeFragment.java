30933124d9280471a732351005f37968a5683fd3
Andy <andynguyen121@gmail.com>
2020-11-02 12:55:27 -0500

    Implement a template for the weather tab

--- HW8-5/app/src/main/java/com/example/mainactivity/ui/home/HomeFragment.java
+++ HW8-5/app/src/main/java/com/example/mainactivity/ui/home/HomeFragment.java
@@ -5,14 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.example.mainactivity.R;
+
 
 public class HomeFragment extends Fragment {
 
